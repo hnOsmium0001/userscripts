ec20ed0d0b9ae154104d316d45ea856e603daa5a
hnOsmium0001 <hn.osmium0001@gmail.com>
2022-07-10 16:26:00 -0700

    Rewrite with preview and markdown support

--- userscripts/irccloud_formatting_helper.user.js
+++ userscripts/irccloud_formatting_helper.user.js
@@ -1,151 +1,216 @@
 // ==UserScript==
 // @name         irccloud formatting helper 2
 // @namespace    http://github.com/hnOsmium0001
-// @version      1.0
+// @version      2.0
 // @description  Utility to add formatting chars for IRCCloud
 // @author       Steve Howard
 // @author       hnOsmium0001
 // @match        https://www.irccloud.com/*
 // @grant        none
 // ==/UserScript==
+
 /* jshint -W097 */
 'use strict';
 
 /**
- * formatHelper: String -> String
- * Converts IRC control characters to HTML tags to preview formatting
+ * Parse a string formatted in (a subset of) Markdown and return a string with HTML format tags.
+ * @param {string} str 
+ * @param {(type: 'bold' | 'italic' | 'underline' | 'strikethrough', transition: boolean) => string} formattingMapper
+ * @returns {string}
  */
-function formatForBrowser (str) {
-    var controlCharRegex = /(\x1d|\x02|\x1e|\x1f)/g;
-    var pieces = str.split(controlCharRegex);
-    var output = [];
+// TODO this currently allows mixing italics formatter '*text_'
+function doFormat(str, formattingMapper) {
+    let bold = false;
+    let italic = false;
+    let strikethrough = false;
+    let underline = false;
 
-    var bold = false, italic = false, strikethrough = false, underline = false;
+    // An array of strings, which may contain text directly 'test' or formatting tags '<b>', '</b>', etc.
+    let resultPieces = [];
 
-    for (var i = 0, end = pieces.length; i < end; ++i) {
-        var piece = pieces[i];
+    let anchor = 0; // Range begin
+    let cursor = 0; // Range end
 
-        if (piece.match(/\x1d/)) {
-            italic = !italic;
-            output.push(italic ? '<i>' : '</i>');
+    /**
+     * @param {number} advance 
+     */
+    function pushTextChunk(advance) {
+        const length = cursor - anchor;
+        if (length > 0) {
+            resultPieces.push(str.substring(anchor, cursor));
         }
-        else if (piece.match(/\x02/)) {
-            bold = !bold;
-            output.push(bold ? '<b>' : '</b>');
-        }
-        else if (piece.match(/\x1e/)) {
-            strikethrough = !strikethrough;
-            output.push(strikethrough ? '<del>' : '</del>');
-        }
-        else if (piece.match(/\x1f/)) {
-            underline = !underline;
-            output.push(underline ? '<u>' : '</u>');
-        }
-        else {
-            output.push(piece);
-        }
+
+        anchor = cursor + advance;
+        // This is taken care of at the end of loop body
+        /* cursor += advance */
     }
 
-    return output.join('');
+    // We don't need to handle surrogate pairs, as long as we transparently keep them intact after the transformation
+    while (cursor < str.length) {
+        // The number after 'c' represents the number of lookahead characters
+        let c0 = str[cursor + 0];
+        let c1 = (cursor < (str.length - 1)) ? str[cursor + 1] : '\0';
+
+        // Number of characters we advanced this pass
+        let advance;
+        let matchedControl = false;
+
+        if (c0 == '*') {
+            if (c1 == '*') {
+                // **text**
+                bold = !bold;
+
+                advance = 2;
+                matchedControl = true;
+
+                pushTextChunk(advance);
+                resultPieces.push(formattingMapper('bold', bold));
+            } else {
+                // *text*
+                italic = !italic;
+
+                advance = 1;
+                matchedControl = true;
+
+                pushTextChunk(advance);
+                resultPieces.push(formattingMapper('italic', italic));
+            }
+        } else if (c0 == '_') {
+            if (c1 == '_') {
+                // __text__
+                underline = !underline;
+
+                advance = 2;
+                matchedControl = true;
+
+                pushTextChunk(advance);
+                resultPieces.push(formattingMapper('underline', underline));
+            } else {
+                // _text_
+                italic = !italic;
+
+                advance = 1;
+                matchedControl = true;
+
+                pushTextChunk(advance);
+                resultPieces.push(formattingMapper('italic', italic));
+            }
+        } else if (c0 == '~' && c1 == '~') {
+            // ~~text~~
+            strikethrough = !strikethrough;
+
+            advance = 2;
+            matchedControl = true;
+
+            pushTextChunk(advance);
+            resultPieces.push(formattingMapper('strikethrough', strikethrough));
+        } else {
+            // We didn't match anything
+            advance = 1;
+            matchedControl = false;
+        }
+
+        cursor += advance;
+    }
+
+    // Push everything else, if there is any
+    cursor = str.length;
+    pushTextChunk(0);
+
+    return resultPieces.join('');
+}
+
+const browserFormattingMap = {
+    'bold': { 'true': '<b>', 'false': '</b>' },
+    'italic': { 'true': '<i>', 'false': '</i>' },
+    'underline': { 'true': '<u>', 'false': '</u>' },
+    'strikethrough': { 'true': '<del>', 'false': '</del>' },
+};
+
+/**
+ * @param {string} str 
+ * @returns {string}
+ */
+function formatMarkdownForBrowser(str) {
+    return doFormat(str, (type, transition) => browserFormattingMap[type][transition]);
+}
+
+const ircFormattingMap = {
+    'bold': { 'true': '\x02', 'false': '\x02' },
+    'italic': { 'true': '\x1d', 'false': '\x1d' },
+    'underline': { 'true': '\x1f', 'false': '\x1f' },
+    'strikethrough': { 'true': '\x1e', 'false': '\x1e' },
+};
+
+/**
+ * @param {string} str 
+ * @returns {string}
+ */
+function formatIrcControlCodes(str) {
+    return doFormat(str, (type, transition) => ircFormattingMap[type][transition]);
 }
 
 /**
- * createOverlay: Node -> Node
- * Creates a div that copies the same
+ * @param {Node} msg
+ * @returns {Node}
  */
-function createOverlay (msg) {
-    var o = document.createElement('div');
-    o.style.width = msg.style.width;
-    o.style.height = msg.style.height;
+function createMessagePreview(msg) {
+    const o = document.createElement('div');
+    // o.style.width = msg.style.width;
+    // o.style.height = msg.style.height;
     o.style.whiteSpace = 'pre';
-
-    o.addEventListener('click', function () {
-        msg.focus();
-    });
-
-    msg.addEventListener('keyup', function () {
-        // Can't just make this a global variable because RegExps store state
-        var controlCharRegex = /(\x1d|\x02|\x1e|\x1f)/g;
-        if (controlCharRegex.test(msg.value)) {
-            // If there were control characters, use the overlay/side by side view to preview message
-            // Easier to check the contents than inspect the state from the above; clears itself when necessary
-            o.innerHTML = formatForBrowser(msg.value);
-            o.style.display = 'block';
-        }
-        else {
-            // otherwise the text box by itself is much faster
-            o.style.display = 'none';
-        }
-    });
-
     return o;
 }
 
-
-function main (msg) {
-    var overlay = createOverlay(msg);
-    msg.parentNode.appendChild(overlay);
-
-    function keyHandler (e) {
-        function emitControlCharacter (c) {
-            var st = msg.selectionStart, en = msg.selectionEnd;
-
-            // check for text selection. If so, insert control character twice
-            if (st < en) {
-                var val = msg.value;
-                msg.value = val.substring(0, st) + c + val.substring(st, en) + c + val.substring(en);
-            }
-            else {
-                msg.value += c;
-            }
-        }
-
-        if (e.ctrlKey) {
-            switch (e.keyCode) {
-                case 73: // 'i'
-                    if (e.altKey) {
-                        emitControlCharacter('\x1d');
-                    }
-                    break;
-
-                case 66: // 'b'
-                    if (e.altKey) {
-                        emitControlCharacter('\x02');
-                    }
-                    break;
-
-                case 85: // 'u'
-                    if (e.altKey) {
-                        emitControlCharacter('\x1f');
-                    }
-                    break;
-
-                case 88: // 'x'
-                    if (e.altKey) {
-                        emitControlCharacter('\x1e');
-                    }
-                    break;
-            }
-        }
+function bindTextarea() {
+    if (cb() == null) {
+        return;
     }
 
-    msg.addEventListener('keyup', keyHandler);
+    // Maintained by IRCCloud, a separate one per buffer
+    const inputBox = document.getElementById(`bufferInputView${cb().bid()}`);
+
+    if (!inputBox.dataset.formattingHelperRegistered) {
+        inputBox.dataset.formattingHelperRegistered = true;
+
+        const previewBox = createMessagePreview();
+        inputBox.parentElement.insertBefore(previewBox, inputBox.nextSibling);    
+
+        inputBox.addEventListener('input', () => {
+            const msg = inputBox.value;
+            const formattedMsg = formatMarkdownForBrowser(msg);
+            previewBox.innerHTML = formattedMsg;
+        }, false);
+
+        inputBox.addEventListener('keydown', (e) => {
+            if (e.key === 'Enter') {
+                const msg = inputBox.value;
+                const formattedMsg = formatIrcControlCodes(msg);
+                inputBox.value = formattedMsg;
+                previewBox.innerHTML = '';
+            }
+        });
+    }
 }
 
-(function () {
-    var initialized = {};
+function init() {
+}
 
-    var observer = new MutationObserver(function (mutations) {
-        mutations.forEach(function (mutation) {
-            Array.from(mutation.addedNodes).forEach(function (node) {
-                var msg = node.querySelector && node.querySelector('[name=msg]');
-                if (msg && !initialized[msg.id]) {
-                    main(msg);
-                    initialized[msg.id] = true;
-                }
+(function checkSession() {
+    // Taken from https://github.com/dogancelik/irccloud-sws/blob/6836cac008/src/send_with_style.user.js#L394-L406
+    if (window.hasOwnProperty('SESSION')) {
+        window.SESSION.bind('init', () => {
+            init();
+
+            // For the initially open channel
+            bindTextarea();
+
+            // For switching channels later (channel == "buffer")
+            window.SESSION.buffers.on('doneSelected', () => {
+                bindTextarea();
             });
         });
-    });
-
-    observer.observe(document.body, { childList: true, subtree: true });
+    } else {
+        setTimeout(checkSession, 100);
+    }
 })();
